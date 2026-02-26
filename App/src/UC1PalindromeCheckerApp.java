53df138460eeff227eeceb85f74b97d36a583d1c
ak1taro <aa4029@srmist.edu.in>
2026-02-26 15:08:50 +0530

    Update UC1PalindromeCheckerApp.java

--- App/src/UC1PalindromeCheckerApp.java
+++ App/src/UC1PalindromeCheckerApp.java
@@ -1,8 +1,38 @@
-// version 1.0
-//author ak1taro
-//useCase 1: Welcome page
-public class PalindromeCheckerApp {
+/**
+ * =============================================================================
+ * MAIN CLASS - UC1PalindromeApp
+ * =============================================================================
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the Palindrome Checker
+ * Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No palindrome logic is implemented yet.
+ * The goal is to establish a clear startup flow.
+ *
+ * @author ak1taro
+ * @version 1.0
+ */
+
+public class UC1PalindromeCheckerApp {
+    /**
+     * Application entry point.
+     *
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @ak1taro args Command-line arguments
+     */
     public static void main(String[] args) {
-            System.out.println("Welcome to Palindrome Checker App Management System.");
+        // Displaying the welcome message and application details
+        System.out.println("Welcome to Palindrome Checker App Management System.");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully..");
     }
 }
